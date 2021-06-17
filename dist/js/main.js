57d178b55824f7df2d0950074859dddd89ad30f8
uchikori <ucchii.wa@gmail.com>
2021-06-17 10:51:42 +0900

    []

---
+++ dist/js/main.js
@@ -0,0 +1,130 @@
+"use strict";
+
+var mySwiper = new Swiper('.main-visual__swiper', {
+  // autoplay:{
+  //     delay:8000,
+  //     stopOnLastSlide: false,
+  //     disableOnInteraction: false,
+  // 	reverseDirection: false
+  // },
+  slidesPerView: 1,
+  speed: 1000,
+  loop: true,
+  shortSwipes: false,
+  longSwipes: false,
+  navigation: {
+    nextEl: '.main-next',
+    prevEl: '.main-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+}); // let mySwiper02 = new Swiper('.student__swiper01',{
+//     spaceBetween: 26,
+//     initialSlide: 1,
+//     slidesPerView: 'auto',
+//     speed:500,
+//     loop:true,
+//     // shortSwipes: false,
+//     // longSwipes: false,
+//     navigation: {
+//         nextEl: '.next02',
+//         prevEl: '.prev02'
+//     },
+// });
+// let mySwiper03 = new Swiper('.student__swiper02',{
+//     spaceBetween: 26,
+//     initialSlide: 1,
+//     slidesPerView: 'auto',
+//     speed:500,
+//     loop:true,
+//     // shortSwipes: false,
+//     // longSwipes: false,
+//     navigation: {
+//         nextEl: '.next03',
+//         prevEl: '.prev03'
+//     },
+// });
+// let mySwiper04 = new Swiper('.student__swiper03',{
+//     spaceBetween: 26,
+//     initialSlide: 1,
+//     slidesPerView: 'auto',
+//     speed:500,
+//     loop:true,
+//     // shortSwipes: false,
+//     // longSwipes: false,
+//     navigation: {
+//         nextEl: '.next04',
+//         prevEl: '.prev04'
+//     },
+// });
+
+var elmSwiper = document.querySelectorAll('.student__swiper');
+var elmPrev = document.querySelectorAll('.prev');
+var elmNext = document.querySelectorAll('.next');
+
+if (elmSwiper.length > 0) {
+  for (var i = 0; i < elmSwiper.length; i++) {
+    elmSwiper[i].className += i;
+    elmPrev[i].className += i;
+    elmNext[i].className += i;
+    var studentSwiper = new Swiper('.student__swiper' + i, {
+      spaceBetween: 26,
+      initialSlide: 1,
+      slidesPerView: 'auto',
+      speed: 500,
+      loop: true,
+      navigation: {
+        prevEl: '.prev' + i,
+        nextEl: '.next' + i
+      }
+    });
+  }
+}
+/**
+ * 
+ * menu-open
+ * 
+ */
+
+
+var humberger = document.querySelector('.humberger');
+var menuToggle = document.querySelector('.global-navi');
+humberger.addEventListener('click', function () {
+  humberger.classList.toggle('menu-active');
+
+  if (humberger.classList.contains('menu-active')) {
+    menuToggle.classList.add('menu-open');
+  } else {
+    menuToggle.classList.remove('menu-open');
+  }
+});
+/**
+ * 
+ * ソート部分反転
+ * 
+ */
+
+jQuery('.js-sort').click(function () {
+  jQuery(this).find('.js-sort-icon').toggleClass('sort-reverse');
+});
+/**
+ * 
+ * Infinite-scroll
+ * 
+ */
+
+var scrollElm = document.querySelector('.student-movie__media-items');
+
+if (scrollElm) {
+  var infiniteScroll = new InfiniteScroll(scrollElm, {
+    path: '.pagination__next',
+    append: '.student-movie__movie-media',
+    status: '.page-load-status',
+    hideNav: '.pagination',
+    button: '.view-more-button',
+    scrollThreshold: false
+  });
+}
